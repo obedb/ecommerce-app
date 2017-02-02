da037b572ea99c363b86e378e9ff5978b32fb3d8
Obed Bigirimana <obedbigirimana@gmail.com>
2017-02-02 15:23:15 +0200

    Added logout to the ecommerce app

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,9 @@ Rails.application.routes.draw do
   get "/signup", to: 'users#new'
   post "/users", to: 'users#create'
 
+  get "/login", to: 'sessions#new'
+  post "/login", to: 'sessions#create'
+  get "/logout", to: 'sessions#destroy'
+  delete ""
+
 end
